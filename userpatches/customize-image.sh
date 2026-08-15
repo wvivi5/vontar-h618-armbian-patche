6ca248e6e652d9a359ea235114bbf7aedf513a4d
wvivi5 <vqiangw@gmail.com>
2026-08-16 05:51:55 +0800

    Update docker-compose package to docker-compose-plugin

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -4,7 +4,7 @@ RELEASE="${1:-}"; LINUXFAMILY="${2:-}"; BOARD="${3:-}"; BUILD_DESKTOP="${4:-}"
 [[ "${BOARD}" == "vontar-h618" ]] || return 0
 export DEBIAN_FRONTEND=noninteractive
 apt-get update || true
-apt-get install -y --no-install-recommends ir-keytable python3-evdev docker.io docker-compose-v2 rsync gdisk e2fsprogs parted util-linux
+apt-get install -y --no-install-recommends ir-keytable python3-evdev docker.io docker-compose-plugin rsync gdisk e2fsprogs parted util-linux
 systemctl enable docker || true
 install -d -m 0755 /etc/rc_keymaps /usr/local/sbin /etc/systemd/system
 for service in vontar-h618-ir.service vontar-h618-power-key.service vontar-h618-migrate.service; do
